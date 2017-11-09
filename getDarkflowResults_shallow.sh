612c15b0713d5c81dbb6dea2e74550c22421c460
hangqiu <hangqiu7@gmail.com>
2017-11-08 20:06:33 -0800

    shallow net

---
+++ getDarkflowResults_shallow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MODEL=cfg/shallow.cfg
+NAMES=../yolo/darknet/data/Bellevue_TimeVary.names
+EVALSET=$1
+RESDIR=$2
+CKPT=$3
+mkdir $RESDIR
+echo flow --imgdir $EVALSET --model $MODEL --restore $CKPT --labels $NAMES --gpu 0.3 --threshold 0.01 --json
+flow --imgdir $EVALSET --model $MODEL --restore $CKPT --labels $NAMES --gpu 0.3 --threshold 0.01 --json
+ls $EVALSET/out/ | sed -e 's/\.json$//'| while read g;do
+	echo python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
+	python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
+done
+rm -r $EVALSET/out/
+
