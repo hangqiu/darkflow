6180050c9eacbb63e27a89decd1ef47280cfc8ac
Hang Qiu <hangqiu7@gmail.com>
2017-11-09 18:44:32 -0800

    nrestore eval

--- getDarkflowResults.sh
+++ getDarkflowResults.sh
@@ -5,9 +5,10 @@ NAMES=../yolo/darknet/data/Bellevue_TimeVary.names
 EVALSET=$1
 RESDIR=$2
 CKPT=$3
+NRESTORE=$4
 mkdir $RESDIR
-echo flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu 0.3 --threshold 0.01 --json
-flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu 0.5 --threshold 0.01 --json
+echo flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu 0.5 --threshold 0.01 --json --nRestore $NRESTORE --nTrain -1
+flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu 0.5 --threshold 0.01 --json --nRestore $NRESTORE --nTrain -1
 ls $EVALSET/out/ | sed -e 's/\.json$//'| while read g;do
 	echo python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
 	python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
