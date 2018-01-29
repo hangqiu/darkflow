bc5c2a468ca5ce7a41ce3aafb383eb8470e4a467
hangqiu <hangqiu7@gmail.com>
2018-01-29 23:38:39 +0000

    remove directory dependency

--- getDarkflowResults_param.sh
+++ getDarkflowResults_param.sh
@@ -6,9 +6,12 @@ EVALSET=$1
 RESDIR=$2
 CKPT=$3
 NRESTORE=$4
+CKPTPATH=$5
+GPU=$6
+
 mkdir $RESDIR
-echo flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu 0.5 --threshold 0.01 --json --nRestore $NRESTORE --nTrain -1
-flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu 0.5 --threshold 0.01 --json --nRestore $NRESTORE --nTrain -1
+echo flow --imgdir $EVALSET --backup $CKPTPATH --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu $GPU --threshold 0.01 --json --nRestore $NRESTORE --nTrain -1
+flow --imgdir $EVALSET --backup $CKPTPATH --model $MODEL --load $WEIGHT --restore $CKPT --labels $NAMES --gpu $GPU --threshold 0.01 --json --nRestore $NRESTORE --nTrain -1
 ls $EVALSET/out/ | sed -e 's/\.json$//'| while read g;do
 	echo python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
 	python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
