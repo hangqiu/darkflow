a6bd44e84915aed97a3b5f9ff6e4bb74b799e3f5
Hang Qiu <hangqiu7@gmail.com>
2018-05-26 16:58:43 -0700

    sync

--- getDarkflowResults_coco.sh
+++ getDarkflowResults_coco.sh
@@ -9,7 +9,7 @@ mkdir $RESDIR
 echo flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --labels $NAMES --gpu 0.1 --threshold 0.01 --json
 flow --imgdir $EVALSET --model $MODEL --load $WEIGHT --labels $NAMES --threshold 0.01 --json
 ls $EVALSET/out/ | sed -e 's/\.json$//'| while read g;do
-	echo python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
-	python3 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
+	echo python3.5 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
+	python3.5 convertRes2Kitti.py --json-res $EVALSET/out/$g.json --txt-res $RESDIR/$g.json.txt
 done
 rm -r $EVALSET/out/
